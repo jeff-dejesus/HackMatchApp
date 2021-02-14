6b418240e6d1dba82bae5c62b84a1585e7771fe2
GregisKhan <35552175+GregisKhan@users.noreply.github.com>
2021-02-14 00:08:38 -0800

    more changes

--- app/src/main/java/com/example/hackermatcher/Dashboard.java
+++ app/src/main/java/com/example/hackermatcher/Dashboard.java
@@ -19,6 +19,6 @@ public class Dashboard extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_login, container, false);
+        return inflater.inflate(R.layout.fragment_dashboard, container, false);
     }
 }
